a19e570f1a3e65e1e5083119e6faa6691567f99a
凌策 <lingce.ldm@alibaba-inc.com>
2016-04-11 22:48:01 +0800

    维护我自己的分支

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
@@ -15,15 +16,16 @@ fi
 sudo easy_install -ZU autopep8 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 mv -f ~/vim ~/vim_old
-cd ~/ && git clone https://github.com/ma6174/vim.git
+cd ~/ && git clone https://github.com/ldming/vim.git
 mv -f ~/.vim ~/.vim_old
 mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "ma6174正在努力为您安装bundle程序" > ma6174
-echo "安装完毕将自动退出" >> ma6174
-echo "请耐心等待" >> ma6174
-vim ma6174 -c "BundleInstall" -c "q" -c "q"
-rm ma6174
+echo "正在努力为您安装bundle程序" > vim_temp
+echo "安装完毕将自动退出" >> vim_temp
+echo "请耐心等待" >> vim_temp
+vim vim_temp -c "BundleInstall" -c "q" -c "q"
+rm vim_temp 
 echo "安装完成"
